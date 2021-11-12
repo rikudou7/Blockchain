8eadf58cf906e933bfe402512bab0434c9651db8
Aaron Watson <52084480+rikudou7@users.noreply.github.com>
2021-11-12 08:16:33 -0500

    First commits

---
+++ config.js
@@ -0,0 +1,5 @@
+const DIFFICULTY = 3; //This is the number of leading zeroes we want our hash to have when mining something like crypto
+const MINE_RATE = 3000; //milliseconds
+const INITIAL_BALANCE = 500; //cryptocurrency
+const MINING_REWARD = 50;
+module.exports = { DIFFICULTY, MINE_RATE, INITIAL_BALANCE, MINING_REWARD };
